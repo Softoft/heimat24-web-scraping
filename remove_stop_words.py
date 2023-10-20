3453959157d0d00dc894840aa9b1ed1d6b46fe54
Tobi Bueck <tobias.bueck@softoft.de>
2023-10-20 15:53:32 +0200

    added complete rosenheim scraped data

---
+++ remove_stop_words.py
@@ -0,0 +1,49 @@
+import re
+
+import nltk
+from bs4 import BeautifulSoup
+
+# Download stopwords
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+
+def count_chars(text):
+    return len(text)
+
+
+def read_html_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
+
+
+def process_html(file_path):
+    text = read_html_file(file_path)
+    soup = BeautifulSoup(text, 'html.parser')
+
+    text = soup.get_text()
+    text = text.lower()
+    lines = text.split('\n')
+    unique_lines = list(set(lines))
+
+    stop_words = set(stopwords.words('english') + stopwords.words('german'))
+    filtered_lines = []
+    for line in unique_lines:
+        words = line.split()
+        filtered_words = [word for word in words if word.lower() not in stop_words]
+        if filtered_words:
+            filtered_lines.append(' '.join(filtered_words))
+
+    text = '\n'.join(filtered_lines)
+    text = re.sub(r'[\s ;.\-_*+#"\']+', ' ', text)
+    return text
+
+
+if __name__ == '__main__':
+    print(stopwords.words('german'))
+    print("{:,.2f}".format(count_chars(read_html_file("website_content.html"))))
+    processed_text = process_html("website_content.html")
+    processed_text = processed_text[:10 ** 7]
+    with open("processed_website_content.html", 'w', encoding='utf-8') as file:
+        file.write(processed_text)
+    print("{:,.2f}".format(count_chars(processed_text)))
